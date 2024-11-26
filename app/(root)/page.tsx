f40c4bd40f163c46f3956fef3e544d4231af4b67
kTz1 <catalin_simon90@yahoo.com>
2024-11-26 16:21:17 +0200

    add SearchForm and SearchFormReset

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -10,6 +15,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote Pitches, and Get Notice in Virtual Competions.
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
