b30d33aa316cc2081eed1f53356c8f1147208fd6
kTz1 <catalin_simon90@yahoo.com>
2024-11-26 14:14:55 +0200

    add hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pich Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote Pitches, and Get Notice in Virtual Competions.
+        </p>
+      </section>
     </>
   );
 }
