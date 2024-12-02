50e67ef9228b02170228c5eeef67a730017e0c7b
kTz1 <catalin_simon90@yahoo.com>
2024-12-02 17:23:30 +0200

    update the number of views

--- components/View.tsx
+++ components/View.tsx
@@ -1,13 +1,23 @@
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  // TODO: Update the number of views
+  // Update the number of views in the background
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
